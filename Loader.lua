fe7a70072dd9605af48d8f2d64afbe9ea67b0f71
Ayna <158354549+AynaShiratori@users.noreply.github.com>
2024-03-06 08:46:29 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -85,7 +85,7 @@ end)
 
 -- Auto Click Attack For Faster Hunt
 task.spawn(function()
-    while task.wait(0.1) do
+    while task.wait(0.33) do
         if getgenv().AutoClick then
              pcall(function()
                 game:GetService'VirtualUser':CaptureController()	                    game:GetService'VirtualUser':Button1Down(Vector2.new(0,1,0,1)) 
