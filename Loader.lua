2fd8707c303652c927139f7d152f0909184567a3
Ayna <158354549+AynaShiratori@users.noreply.github.com>
2024-03-13 04:19:26 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,6 +5,16 @@ repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
 repeat task.wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
 task.wait(3)
 
+
+getgenv().Hermanos_Settings = {
+    ['key'] = '4a97a4c2-cac7-46ff-8009-332ccf4caef6',
+
+    ['PC'] = 'PC-1',
+    ['Sword'] = {''},
+    ['Fruit'] = {'Kitsune'},
+}
+task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/script.lua'))() end)
+
 -- Better Performance
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 settings().Rendering.QualityLevel = "1"
