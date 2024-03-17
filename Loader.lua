7e9bfa0dd77c456445c0f2a904cd7fbc09abadc1
Ayna <158354549+AynaShiratori@users.noreply.github.com>
2024-03-17 13:45:21 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -57,8 +57,8 @@ getgenv().config = {
             }
         },
         ["FPS Locker"] = {
-            ["Enabled"] = false,
-            ["Value"] = 12
+            ["Enabled"] = true,
+            ["Value"] = 30
         },
         ["Bounty Lock"] = {
             ["Enabled"] = false,
