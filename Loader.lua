c5c096f9bc3f84eeac553c3512e467bbd6fa42dd
Ayna <158354549+AynaShiratori@users.noreply.github.com>
2024-03-18 00:17:46 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,4 @@
+setfpscap(30)
 task.wait(10)
 getgenv().config = {
     ["Team"] = "Pirates",
@@ -33,8 +34,8 @@ getgenv().config = {
             }
         },
         ["FPS Locker"] = {
-            ["Enabled"] = true,
-            ["Value"] = 25
+            ["Enabled"] = false,
+            ["Value"] = 30
         },
         ["Bounty Lock"] = {
             ["Enabled"] = false,
@@ -104,8 +105,195 @@ repeat task.wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main
 
 -- Auto Bounty
 task.spawn(function()
-    task.wait(3)
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/LeNguyenBaoPhuc/BloxFruits/main/Bounty.lua")))()
+end)
+
+function ChooseTeam()
+	loadstring(game:HttpGet("https://gist.githubusercontent.com/AynaShiratori/1753c889c3be4c41853a847c81f15c8e/raw/b3c71ff6b1fad5a2820dbba699e6cee45ee89fdc/ChooseTeam"))()
+end
+
+-- If Current Sea Is Sea 1 Then Teleport To Sea2
+if game.PlaceId == 2753915549 then
+    ChooseTeam()
+    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("TravelDressrosa")
+end
+
+-- If Current Sea Is Sea 2 Then Teleport To Sea3
+
+if game.PlaceId == 4442272183 then
+    ChooseTeam()
+    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("TravelZou")
+end
+
+-- Performance
+settings().Rendering.QualityLevel = "1"
+UserSettings():GetService("UserGameSettings").MasterVolume = 0
+game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.Chat,false)
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
+local CameraShake = require(game.ReplicatedStorage.Util.CameraShaker)
+CameraShake:Stop()
+game.Players.LocalPlayer.PlayerGui.Notifications.Enabled = false
+game.Players.LocalPlayer.PlayerGui.TopbarPlus.Enabled = false
+game.Players.LocalPlayer.PlayerGui.Main.DynamicTopBar:Destroy()
+game:GetService("Players").LocalPlayer.PlayerGui.TouchGui:Destroy()
+game:GetService("Players").LocalPlayer.PlayerGui.MobileMouselock:Destroy()
+
+game.Workspace:WaitForChild"Terrain".WaterWaveSize = 0
+game.Workspace:WaitForChild"Terrain".WaterWaveSpeed = 0
+game.Workspace:WaitForChild"Terrain".WaterReflectance = 0
+game.Workspace:WaitForChild"Terrain".WaterTransparency = 1
+game:GetService"Lighting".GlobalShadows = false
+game:GetService("Lighting"):ClearAllChildren()
+
+task.spawn(function()
+    game.Players.LocalPlayer.PlayerScripts.WaterCFrame.Disabled = true
+end)
+task.spawn(function()
+    game.Players.LocalPlayer.PlayerScripts.EnhancementVisual.Disabled = true
+end)
+for i,v in pairs(game:GetService("Workspace").Map:GetDescendants()) do
+    if (v:IsA("Part") or v:IsA("MeshPart") or v:IsA("BasePart")) then
+        v:Remove()
+    end
+end
+game.Workspace.Enemies:Remove()
+game.Workspace.SeaBeasts:Remove()
+game.Workspace.SeaEvents:Remove()
+game.Workspace.Boats:Remove()
+game.Workspace.Leaderboards:Remove()
+game:GetService("ReplicatedStorage").Effect.Container:Remove()
+task.spawn(function()
+    game:GetService("ReplicatedStorage").FX:Remove()
+end)
+game:GetService("ReplicatedStorage").Assets:Remove()
+game:GetService("ReplicatedStorage").ClientWeapons:Remove()
+
+-- Remove Effects
+task.spawn(function()
+    while task.wait() do
+        for i, v in pairs(game:GetService("Workspace")["_WorldOrigin"]:GetChildren()) do
+            pcall(function()
+                if v.Name == ("CurvedRing") or v.Name == ("SlashHit") or v.Name == ("SwordSlash") or v.Name == ("SlashTail") then
+                    v:Remove()
+                end
+            end)
+        end
+    end
+end)
+
+-- Equip Dragon Talon, Warrior Helmet And Soul Guitar
+task.spawn(function()
+    while task.wait(1) do
+        if not game.Players.LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam") then
+            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyDragonTalon")
+            task.wait(0.1)
+            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("LoadItem","Warrior Helmet")
+            task.wait(0.1)
+            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("LoadItem","Soul Guitar")
+            break
+        end
+    end
+end)
+
+-- Auto Purchase Chop Fruit
+task.spawn(function()
+    pcall(function()
+        while wait(1) do
+            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("GetFruits")
+            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("PurchaseRawFruit","Chop-Chop",false)
+        end
+    end)
+end)
+
+-- Check If Bounty Is Not Changed
+local AFKDelayCheck = 600
+task.spawn(function()
+    local PlayerAbcdf = game.Players.LocalPlayer
+    local currentBountyAbcdf = game:GetService("Players").LocalPlayer.leaderstats["Bounty/Honor"].Value
+    print("Current Bounty:", currentBountyAbcdf)
+    while true do
+        wait(AFKDelayCheck)
+        print("Checking Bounty")
+        if PlayerAbcdf then
+            local newBountyAbcdf = game:GetService("Players").LocalPlayer.leaderstats["Bounty/Honor"].Value
+            if (newBountyAbcdf == currentBountyAbcdf and currentBountyAbcdf > 1) and game.Players.LocalPlayer.PlayerGui.InCombat.Visible == false then
+                print("Bounty Is Stucked, Server Hopping...")
+                game.Players.LocalPlayer:Kick("Detected Not Killing/AFK, Server Hopping...")
+            else
+                currentBountyAbcdf = newBountyAbcdf
+            end
+        end
+    end
+end)
+
+-- If Current Bounty Is 30M Then Shut Down The Game
+task.spawn(function()
+    while task.wait() do
+        if game:GetService("Players").LocalPlayer.leaderstats["Bounty/Honor"].Value >= 30000000 then
+            game:Shutdown()
+        end
+    end
+end)
+
+-- Check If Auto-Bounty Is Loaded
+repeat
+    task.wait()
+    if not game.Players.LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam") then
+        break
+    end
+    wait(1)
+until not game.Players.LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam")
+task.wait(1)
+
+-- Track Stat
+getgenv().Hermanos_Settings = {
+    ['key'] = '4a97a4c2-cac7-46ff-8009-332ccf4caef6',
+
+    ['PC'] = '1',
+    ['Sword'] = {'',},
+    ['Fruit'] = {'Kitsune'},
+}
+task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/script.lua'))() end)
+
+-- Teleport To Sea3
+game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("TravelZou")
+
+-- Better Vision
+task.spawn(function()
+	while task.wait(0.5) do
+		game.Players.LocalPlayer.PlayerGui.Main.Compass.Visible = false
+		game.Players.LocalPlayer.PlayerGui.Main.CrewButton.Visible = false
+	end
+end)
+game.Players.LocalPlayer.PlayerGui.Main.ShopButton.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.Shop.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.Settings.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.Mute.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.HomeButton.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.AlliesButton.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.Code.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.InventoryContainer.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.Stats.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.Allies.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.Beli.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.Fragments.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.Level.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.MenuButton.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.Energy.Visible = false
+game.Players.LocalPlayer.PlayerGui.Main.InCombat.TextTransparency = 1
+game.Players.LocalPlayer.PlayerGui.Main.InCombat.Bottom.TextTransparency = 1
+game.Workspace.NPCs:Remove()
+task.spawn(function()
+    game.Players.LocalPlayer.PlayerGui.ContextActionGui.Enabled = false
+end)
+-- Remove Godx Auto Bounty Ui
+task.spawn(function()
+    while task.wait() do
+        if game.Players.LocalPlayer.PlayerGui:FindFirstChild("BountyTracker") then
+            game.Players.LocalPlayer.PlayerGui.BountyTracker:Destroy()
+            break
+        end
+    end
 end)
 
 -- Status
@@ -319,192 +507,4 @@ task.spawn(function()
 		end
 		timerlabel.Text = DisplayedHours..":"..DisplayedMinutes..":"..DisplayedSeconds
 	end
-end)
-
-function ChooseTeam()
-	loadstring(game:HttpGet("https://gist.githubusercontent.com/AynaShiratori/1753c889c3be4c41853a847c81f15c8e/raw/b3c71ff6b1fad5a2820dbba699e6cee45ee89fdc/ChooseTeam"))()
-end
-
--- If Current Sea Is Sea 1 Then Teleport To Sea2
-if game.PlaceId == 2753915549 then
-    ChooseTeam()
-    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("TravelDressrosa")
-end
-
--- If Current Sea Is Sea 2 Then Teleport To Sea3
-
-if game.PlaceId == 4442272183 then
-    ChooseTeam()
-    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("TravelZou")
-end
-
--- Performance
-settings().Rendering.QualityLevel = "1"
-UserSettings():GetService("UserGameSettings").MasterVolume = 0
-game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.Chat,false)
-game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
-local CameraShake = require(game.ReplicatedStorage.Util.CameraShaker)
-CameraShake:Stop()
-game.Players.LocalPlayer.PlayerGui.Notifications.Enabled = false
-game.Players.LocalPlayer.PlayerGui.TopbarPlus.Enabled = false
-game.Players.LocalPlayer.PlayerGui.Main.DynamicTopBar:Destroy()
-game:GetService("Players").LocalPlayer.PlayerGui.TouchGui:Destroy()
-game:GetService("Players").LocalPlayer.PlayerGui.MobileMouselock:Destroy()
-
-game.Workspace:WaitForChild"Terrain".WaterWaveSize = 0
-game.Workspace:WaitForChild"Terrain".WaterWaveSpeed = 0
-game.Workspace:WaitForChild"Terrain".WaterReflectance = 0
-game.Workspace:WaitForChild"Terrain".WaterTransparency = 1
-game:GetService"Lighting".GlobalShadows = false
-game:GetService("Lighting"):ClearAllChildren()
-
-task.spawn(function()
-    game.Players.LocalPlayer.PlayerScripts.WaterCFrame.Disabled = true
-end)
-task.spawn(function()
-    game.Players.LocalPlayer.PlayerScripts.EnhancementVisual.Disabled = true
-end)
-for i,v in pairs(game:GetService("Workspace").Map:GetDescendants()) do
-    if (v:IsA("Part") or v:IsA("MeshPart") or v:IsA("BasePart")) then
-        v:Remove()
-    end
-end
-game.Workspace.Enemies:Remove()
-game.Workspace.SeaBeasts:Remove()
-game.Workspace.SeaEvents:Remove()
-game.Workspace.Boats:Remove()
-game.Workspace.Leaderboards:Remove()
-game:GetService("ReplicatedStorage").Effect.Container:Remove()
-task.spawn(function()
-    game:GetService("ReplicatedStorage").FX:Remove()
-end)
-game:GetService("ReplicatedStorage").Assets:Remove()
-game:GetService("ReplicatedStorage").ClientWeapons:Remove()
-
--- Remove Effects
-task.spawn(function()
-    while task.wait() do
-        for i, v in pairs(game:GetService("Workspace")["_WorldOrigin"]:GetChildren()) do
-            pcall(function()
-                if v.Name == ("CurvedRing") or v.Name == ("SlashHit") or v.Name == ("SwordSlash") or v.Name == ("SlashTail") then
-                    v:Remove()
-                end
-            end)
-        end
-    end
-end)
-
--- Equip Dragon Talon, Warrior Helmet And Soul Guitar
-task.spawn(function()
-    while task.wait(1) do
-        if not game.Players.LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam") then
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyDragonTalon")
-            task.wait(0.1)
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("LoadItem","Warrior Helmet")
-            task.wait(0.1)
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("LoadItem","Soul Guitar")
-            break
-        end
-    end
-end)
-
--- Auto Purchase Chop Fruit
-task.spawn(function()
-    pcall(function()
-        while wait(1) do
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("GetFruits")
-            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("PurchaseRawFruit","Chop-Chop",false)
-        end
-    end)
-end)
-
--- Check If Bounty Is Not Changed
-local AFKDelayCheck = 600
-task.spawn(function()
-    local PlayerAbcdf = game.Players.LocalPlayer
-    local currentBountyAbcdf = game:GetService("Players").LocalPlayer.leaderstats["Bounty/Honor"].Value
-    print("Current Bounty:", currentBountyAbcdf)
-    while true do
-        wait(AFKDelayCheck)
-        print("Checking Bounty")
-        if PlayerAbcdf then
-            local newBountyAbcdf = game:GetService("Players").LocalPlayer.leaderstats["Bounty/Honor"].Value
-            if (newBountyAbcdf == currentBountyAbcdf and currentBountyAbcdf > 1) and game.Players.LocalPlayer.PlayerGui.InCombat.Visible == false then
-                print("Bounty Is Stucked, Server Hopping...")
-                game.Players.LocalPlayer:Kick("Detected Not Killing/AFK, Server Hopping...")
-            else
-                currentBountyAbcdf = newBountyAbcdf
-            end
-        end
-    end
-end)
-
--- If Current Bounty Is 30M Then Shut Down The Game
-task.spawn(function()
-    while task.wait() do
-        if game:GetService("Players").LocalPlayer.leaderstats["Bounty/Honor"].Value >= 30000000 then
-            game:Shutdown()
-        end
-    end
-end)
-
--- Check If Auto-Bounty Is Loaded
-repeat
-    task.wait()
-    if not game.Players.LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam") then
-        break
-    end
-    wait(1)
-until not game.Players.LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam")
-task.wait(1)
-
--- Track Stat
-getgenv().Hermanos_Settings = {
-    ['key'] = '4a97a4c2-cac7-46ff-8009-332ccf4caef6',
-
-    ['PC'] = '1',
-    ['Sword'] = {'',},
-    ['Fruit'] = {'Kitsune'},
-}
-task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/script.lua'))() end)
-
--- Teleport To Sea3
-game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("TravelZou")
-
--- Better Vision
-task.spawn(function()
-	while task.wait(0.5) do
-		game.Players.LocalPlayer.PlayerGui.Main.Compass.Visible = false
-		game.Players.LocalPlayer.PlayerGui.Main.CrewButton.Visible = false
-	end
-end)
-game.Players.LocalPlayer.PlayerGui.Main.ShopButton.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.Shop.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.Settings.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.Mute.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.HomeButton.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.AlliesButton.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.Code.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.InventoryContainer.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.Stats.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.Allies.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.Beli.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.Fragments.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.Level.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.MenuButton.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.Energy.Visible = false
-game.Players.LocalPlayer.PlayerGui.Main.InCombat.TextTransparency = 1
-game.Players.LocalPlayer.PlayerGui.Main.InCombat.Bottom.TextTransparency = 1
-game.Workspace.NPCs:Remove()
-task.spawn(function()
-    game.Players.LocalPlayer.PlayerGui.ContextActionGui.Enabled = false
-end)
--- Remove Godx Auto Bounty Ui
-task.spawn(function()
-    while task.wait() do
-        if game.Players.LocalPlayer.PlayerGui:FindFirstChild("BountyTracker") then
-            game.Players.LocalPlayer.PlayerGui.BountyTracker:Destroy()
-            break
-        end
-    end
 end)
