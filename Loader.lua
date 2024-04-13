6d67b4542be9860a3df496f07859b0b00ebd80e7
Ayna <158354549+AynaShiratori@users.noreply.github.com>
2024-04-14 04:23:55 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1076,6 +1076,7 @@ if isfile("AynaShiratori/IsFarmingCursedDualKatanaMastery/" .. game.Players.Loca
     LoadWazureHubCursedDualKatanaMasteryFarmConfig()
     ChooseTeam()
     LoadWazureHub()
+    task.wait(10)
     FullyFPSBooster()
 end
 
