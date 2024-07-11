f9a27e9187d2c657ed5883af4d94857aa542e9c3
Ayna <158354549+AynaShiratori@users.noreply.github.com>
2024-07-11 19:54:58 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -41,13 +41,58 @@ if not ScriptKey == "Ayna Shiratori 1" then
 end
 
 -- Wazure Settings ( Including Hub And Auto Bounty V2)
+-- Wazure Settings ( Including Hub And Auto Bounty V2.2)
 script_key = "WinPShtjsOjPMYIuyeldlfhLfxJtkDWo"
 getgenv().Team = "Pirates"
-getgenv().AutoLoad = false
-getgenv().SlowLoadUi  = false
-getgenv().ForceUseSilentAimDashModifier = false
-getgenv().ForceUseWalkSpeedModifier = false
-getgenv().Team = "Pirates"
+getgenv().WebhookSetting = {
+    Enable = true,
+    Url = "",
+    Embed = true,
+    StoredFruit = true,
+    ImageEmbed = true,
+    CustomImage = false,
+    CustomImageUrl = "", --Your Url
+    OnServerHop = true,
+    BountyChanged = true,
+}
+getgenv().PlayerSetting = {
+    SafeMode = true,
+    SafeModeHealth = {3000,75},--Number And %, Start Safe Mode And Stop Safe Mode
+    UseRaceV3 = true,
+    SmartUseRaceV3= true,
+    DashIfV4 = false,
+    Dash=true,
+    IgnoreInCombat = true, --Turn This Off When Reseting Or Hop You Lost Bounty (Rare, Happens On Some Accounts)
+    ChatKillEnable = false,
+    Chat = {"Ez","You're just too bad"},
+    IgnoreFriends = false, --Serverhop if you friend in your server
+}
+getgenv().AttackSetting = {
+    ForceMelee = true,
+    ForceMeleeTime = 3.5,
+    StopAttack =true, --When Meet Below Condition
+    StopAttackAtHealth = 79,--%
+    FastAttack=true, -- Toggle Fast Attack
+}
+getgenv().UseSkillSetting = {
+    -- Three Methods: "Normal", "Fast", "Spam", "SpamAll"
+    MethodIfTargetOnV4 = "Fast",
+    MethodIfPlayerOnV4 = "SpamAll",
+    MethodIfTargetUseFruit = {Fruits={},Method="Fast"},
+    NormalMethod = "Normal",
+    LowHealthPlayerCondition = { --Player Can Attack Us, No Need For Slow Attack
+        Enable = true,
+        Health = 70,--%Health That Are Low
+        Method = "Fast",
+    },
+    LowHealthTargetCondition = {
+        Enable = true,
+        Health = 40,--%Health That Are Low
+        DelayFirstTime = {true,2}, --1 Is Enable, 2 Is Second To Delay Before Attack Again
+        Method = "Normal",
+        WaitTime = 1.5,-- If Normal Method, Wait Every Skill If It Hits Target
+    }
+}
 getgenv().WeaponsSetting = {
     ["Melee"] = {
         ["Enable"] = true,
@@ -56,19 +101,19 @@ getgenv().WeaponsSetting = {
         ["Skills"] = {
             ["Z"] = {
                 ["Enable"] = true,
-                ["NoPredict"] = false,
-                ["HoldTime"] = 0.11,
+                ["NoPredict"] = false, -- For Dragon Tailon, Disable it 
+                ["HoldTime"] = 0.4,
                 ["TimeToNextSkill"] = 0,
             },
         [ "X"] = {
                 ["Enable"] = true,
-                ["HoldTime"] = 0.14,
+                ["HoldTime"] = 0,
                 ["TimeToNextSkill"] = 0,
             },
 
             ["C"] = {
                 ["Enable"] = true,
-                ["HoldTime"] = 0.8,
+                ["HoldTime"] = 0.3,
                 ["TimeToNextSkill"] = 0,
             },
         },
@@ -76,31 +121,31 @@ getgenv().WeaponsSetting = {
     ["Blox Fruit"] = {
         ["Enable"] = false,
         ["Delay"] = 4,
-        ["SwitchNextWeaponIfCooldown"] = true,
+        ["SwitchNextWeaponIfCooldown"] = false,
         ["Skills"] = {
             ["Z"] = {
-                ["Enable"] = true,
+                ["Enable"] = false,
                 ["HoldTime"] = 2,
                 ["TimeToNextSkill"] = 0,
             },
             ["X"] = {
-                ["Enable"] = true,
-                ["HoldTime"] = 0.3,
+                ["Enable"] = false,
+                ["HoldTime"] = 0,
                 ["TimeToNextSkill"] = 0,
             },
 
             ["C"] = {
-                ["Enable"] = true,
-                ["HoldTime"] = 0.3,
+                ["Enable"] = false,
+                ["HoldTime"] = 0,
                 ["TimeToNextSkill"] = 0,
             },
             ["V"] = {
-                ["Enable"] = true,
-                ["HoldTime"] = 0.3,
+                ["Enable"] = false,
+                ["HoldTime"] = 0,
                 ["TimeToNextSkill"] = 0,
             },
             ["F"] = {
-                ["Enable"] = true,
+                ["Enable"] = false,
                 ["HoldTime"] = 0,
                 ["TimeToNextSkill"] = 0,
             },
@@ -108,16 +153,16 @@ getgenv().WeaponsSetting = {
     },
     ["Sword"] = {
         ["Enable"] = false,
-        ["Delay"] = 0.5,
+        ["Delay"] = 1.2,
         ["Skills"] = {
             ["Z"] = {
                 ["Enable"] = true,
-                ["HoldTime"] = 1.2,
+                ["HoldTime"] = 1.5,
                 ["TimeToNextSkill"] = 0,
             },
             ["X"] = {
                 ["Enable"] = true,
-                ["HoldTime"] = 0.5,
+                ["HoldTime"] = 1,
                 ["TimeToNextSkill"] = 0,
             },
         },
@@ -127,17 +172,31 @@ getgenv().WeaponsSetting = {
         ["Delay"] = 0.5,
         ["Skills"] = {
             ["Z"] = {
-                ["Enable"] = false,
-                ["HoldTime"] = 0.1,
+                ["Enable"] = true,
+                ["HoldTime"] = 0,
                 ["TimeToNextSkill"] = 0,
             },
             ["X"] = {
                 ["Enable"] = true,
-                ["HoldTime"] = 0.1,
+                ["HoldTime"] = 0,
                 ["TimeToNextSkill"] = 0,
             },
         },
     },
+}
+getgenv().Theme = { -- getgenv().Theme = false if you want to disable
+    OldTheme = false,
+    Name="Miko", --"Raiden","Ayaka","Hutao","Yelan","Miko","Nahida","Ganyu","Keqing","Nilou","Barbara","Zhongli","Layla"
+    Custom={
+            ["Enable"] = true,
+            ['char_size'] = UDim2.new(0.668, 0, 1.158, 0),
+            ['char_pos'] = UDim2.new(0.463, 0, -0.105, 0),
+            ['title_color'] = Color3.fromRGB(255, 221, 252),
+            ['titleback_color'] = Color3.fromRGB(169, 20, 255),
+            ['list_color'] = Color3.fromRGB(255, 221, 252),
+            ['liststroke_color'] = Color3.fromRGB(151, 123, 207),
+            ['button_color'] = Color3.fromRGB(255, 221, 252)
+       }
 }
 
 
@@ -757,7 +816,7 @@ end
 
 function LoadWazureAutoBounty()
     task.spawn(function()
-        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b6d24ef1f7dab9c7b22f259a3db6c47e.lua"))()
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/248f97d7a28a4d09c641d8279a935333.lua"))()
     end)
 end
 
@@ -1317,7 +1376,7 @@ task.spawn(function()
                 mousemoveabs(workspace.CurrentCamera.ViewportSize.X / 2, workspace.CurrentCamera.ViewportSize.Y / 2.3)
             end
         end)
-        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("LoadItem","Tomoe Ring")
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("LoadItem","Warrior Helmet")
         game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("BuyDragonTalon")
     end
 end)
